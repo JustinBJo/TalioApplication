39e47dbcd00c16c9913f520531769a7c7ebe390b
Francisco Cunha <F.SiqueiraCarneirodaCunhaNeto-1@student.tudelft.nl>
2023-03-07 19:19:57 +0100

    Create Board JPA entity and placeholder TaskList

---
+++ commons/src/main/java/commons/TaskList.java
@@ -0,0 +1,16 @@
+package commons;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TaskList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    public TaskList() {
+    }
+}
