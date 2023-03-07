bb0d7a021da06e969da80f46cb0be1a87c18484f
Renyi Yang <R.Yang-7@student.tudelft.nl>
2023-03-07 19:22:30 +0100

    fix: color serialize

--- server/src/main/java/server/api/ThemeController.java
+++ server/src/main/java/server/api/ThemeController.java
@@ -1,11 +1,14 @@
 package server.api;
 
 import commons.Theme;
+import model.ThemeRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.database.ThemeRepository;
+
+import java.awt.*;
 
 @RestController
 @RequestMapping("/themes")
@@ -15,7 +18,12 @@ public class ThemeController {
     private ThemeRepository themeRepository;
 
     @PostMapping("/")
-    public ResponseEntity<Theme> createTheme(@RequestBody Theme theme) {
+    public ResponseEntity<Theme> createTheme(@RequestBody ThemeRequestModel themeRequestModel) {
+        Color board = themeRequestModel.getBoardColor();
+        Color list = themeRequestModel.getListColor();
+        Color task = themeRequestModel.getTaskColor();
+        Color subtask = themeRequestModel.getSubtaskColor();
+        Theme theme = new Theme(board, list, task, subtask);
         Theme savedTheme = themeRepository.save(theme);
         return new ResponseEntity<>(savedTheme, HttpStatus.CREATED);
     }
