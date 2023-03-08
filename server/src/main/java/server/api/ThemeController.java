d2c4ca94499baf3c1f88658de8408a3564ac424f
Renyi Yang <R.Yang-7@student.tudelft.nl>
2023-03-08 23:40:45 +0100

    checkstyle

--- server/src/main/java/server/api/ThemeController.java
+++ server/src/main/java/server/api/ThemeController.java
@@ -2,7 +2,6 @@ package server.api;
 
 import commons.Theme;
 import model.ThemeRequestModel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
